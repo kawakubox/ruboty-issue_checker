a5e3fb4bf615f8d134734e8125f028a5960df5f2
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-27 20:09:30 +0900

    Modify command regexp.

--- lib/ruboty/hundlers/issue_checker.rb
+++ lib/ruboty/hundlers/issue_checker.rb
@@ -3,7 +3,7 @@ require 'ruboty/issue_checker/actions/patrol'
 module Ruboty
   module hundlers
     class IssueChecker < Base
-      on /patrol (?<repo>.*)\z/, name: 'patrol', description: 'Github issue patrol.'
+      on /issue patrol (?<repo>.*)\z/, name: 'patrol', description: 'Github issue patrol.'
 
       def patrol(message)
         Ruboty::IssueChecker::Actions::Patrol.new(message).call
