53195fd0236906deba919223f64e6d0b90a73150
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-27 21:17:10 +0900

    Add hoge.

--- lib/ruboty/hundlers/issue_checker.rb
+++ lib/ruboty/hundlers/issue_checker.rb
@@ -4,9 +4,14 @@ module Ruboty
   module hundlers
     class IssueChecker < Base
       on /issue patrol (?<repo>.*)\z/, name: 'patrol', description: 'Github issue patrol.'
+      on /hoge\z/, name: 'hoge', description: 'Hoge.'
 
       def patrol(message)
         Ruboty::IssueChecker::Actions::Patrol.new(message).call
+      end
+
+      def hoge
+        'Hoge Hoge.'
       end
     end
   end
