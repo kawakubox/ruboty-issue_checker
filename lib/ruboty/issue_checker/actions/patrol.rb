2ed5b3915383662716064a0924ac14ddf32e2430
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-29 07:39:42 +0900

    Add octokit.

--- lib/ruboty/issue_checker/actions/patrol.rb
+++ lib/ruboty/issue_checker/actions/patrol.rb
@@ -1,9 +1,18 @@
+require 'octokit'
+
 module Ruboty
   module IssueChecker
     module Actions
       class Patrol < Ruboty::Actions::Base
         def call
-          message.reply('Not implemented.')
+          client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+          message.reply ENV['GITHUB_TOKEN']
+          if client.repository?(message[:repo])
+            repo = client.list_issues(message[:repo])
+            message.reply(repo.to_s)
+          else
+            message.reply("Repository(#{message[:repo]}) is not found.")
+          end
         end
       end
     end
