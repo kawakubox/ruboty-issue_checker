aa5712c6e306c1d1d6f5f778dfd1916abda0ddb3
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-29 17:44:34 +0900

    Remove handler for 'hoge'.

--- lib/ruboty/handlers/issue_checker.rb
+++ lib/ruboty/handlers/issue_checker.rb
@@ -4,14 +4,9 @@ module Ruboty
   module Handlers
     class IssueChecker < Base
       on /issue patrol (?<repo>.*)\z/, name: 'patrol', description: 'Github issue patrol.'
-      on /hoge\z/, name: 'hoge', description: 'Hoge.'
 
       def patrol(message)
         Ruboty::IssueChecker::Actions::Patrol.new(message).call
-      end
-
-      def hoge
-        'Hoge Hoge.'
       end
     end
   end
