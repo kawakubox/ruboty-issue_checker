6dc4708840f02a5ba905ee0ccfdb73f2747f4075
kawakubox <shigefumi.kawakubo@gmail.com>
2016-02-03 20:37:26 +0900

    Skip pull request issue.

--- lib/ruboty/issue_checker/actions/patrol.rb
+++ lib/ruboty/issue_checker/actions/patrol.rb
@@ -8,8 +8,12 @@ module Ruboty
           client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
           message.reply ENV['GITHUB_TOKEN']
           if client.repository?(message[:repo])
-            repo = client.list_issues(message[:repo])
-            message.reply(repo.to_s)
+            issues = client.list_issues(message[:repo])
+            issues.each do |issue|
+              next if issue.pull_request
+              p issue.html_url unless issue.assignee
+              p issue.html_url unless issue.milestone
+            end
           else
             message.reply("Repository(#{message[:repo]}) is not found.")
           end
